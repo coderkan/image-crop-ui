8803831f005b501fa03015b3613e2dba15bd80ac
Erkan Güzeler <erkanguzeler@gmail.com>
2016-10-29 00:21:00 +0300

    added Change Background image activity

--- app/src/main/java/cheetatech/ucropcustomui/MainActivity.java
+++ app/src/main/java/cheetatech/ucropcustomui/MainActivity.java
@@ -1,5 +1,6 @@
 package cheetatech.ucropcustomui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ((ImageView)findViewById(R.id.backgroundIconChange)).setOnClickListener(this);
 
 
 
@@ -45,5 +49,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.backgroundIconChange:
+                Intent intent = new Intent(this, ChangeBackground.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
