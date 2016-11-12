bc4696927d8b8efeb2857b8bd5845e743813411d
Erkan Güzeler <erkanguzeler@gmail.com>
2016-11-13 01:02:31 +0300

    added anonymously firebase sign in

--- app/src/main/java/cheetatech/ucropcustomui/ImageLoadActivity.java
+++ app/src/main/java/cheetatech/ucropcustomui/ImageLoadActivity.java
@@ -1,6 +1,7 @@
 package cheetatech.ucropcustomui;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,8 +10,13 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -29,11 +35,14 @@ public class ImageLoadActivity extends AppCompatActivity {
     @BindView(R.id.my_recycler_view)
     RecyclerView mRecyclerView;
 
+
+
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager  mLayoutManager;
 
 
-    private FirebaseAuth mAuth;
+    private FirebaseAuth mAuth = null;
+    private FirebaseAuth.AuthStateListener mAuthListener = null;
     private FirebaseStorage storage = null;
     private StorageReference storageRef = null;
 
@@ -50,44 +59,44 @@ public class ImageLoadActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-        mAuth = FirebaseAuth.getInstance();
-
-        storage = FirebaseStorage.getInstance();
-        if(storage == null)
-            Log.e(TAG,"storage null");
-        storageRef = storage.getReferenceFromUrl(BUCKETPATH);
-        if(storageRef == null)
-            Log.e(TAG,"storage REF null");
-
-        StorageReference reference = storageRef.child("images");
-
-        if(reference == null)
-            Log.e(TAG,"REF null");
+        controlAuth();
 
 
+//        storage = FirebaseStorage.getInstance();
+//        if(storage == null)
+//            Log.e(TAG,"storage null");
+//        storageRef = storage.getReferenceFromUrl(BUCKETPATH);
+//        if(storageRef == null)
+//            Log.e(TAG,"storage REF null");
+//
+//        StorageReference reference = storageRef.child("images");
+//
+//        if(reference == null)
+//            Log.e(TAG,"REF null");
 
 
 
 
-        ArrayList<String> list = new ArrayList<String>();
-        for(int i = 0; i < 20; i++)
-            list.add("Erkan " + i);
 
-        ArrayList<File> fileList = (ArrayList<File>) FileUtilz.getListFiles(getApplicationContext(), Side.BACKGROUND);
 
-        for (File file: fileList
-             ) {
-            Log.e("TAG","Name "+ file.getAbsolutePath().toString());
-        }
-
-        mRecyclerView.setHasFixedSize(true);
-
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        mAdapter = new MyAdapter(fileList);
-        mRecyclerView.setAdapter(mAdapter);
+//        ArrayList<String> list = new ArrayList<String>();
+//        for(int i = 0; i < 20; i++)
+//            list.add("Erkan " + i);
+//
+//        ArrayList<File> fileList = (ArrayList<File>) FileUtilz.getListFiles(getApplicationContext(), Side.BACKGROUND);
+//
+//        for (File file: fileList
+//             ) {
+//            Log.e("TAG","Name "+ file.getAbsolutePath().toString());
+//        }
+//
+//        mRecyclerView.setHasFixedSize(true);
+//
+//        mLayoutManager = new LinearLayoutManager(this);
+//        mRecyclerView.setLayoutManager(mLayoutManager);
+//
+//        mAdapter = new MyAdapter(fileList);
+//        mRecyclerView.setAdapter(mAdapter);
 
 
 
@@ -95,10 +104,77 @@ public class ImageLoadActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //signIn();
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void signIn(){
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.e(TAG,"signInAnonymously:onComplete: "+ task.isSuccessful());
+                        if(!task.isSuccessful()){
+                            Log.e(TAG,"signInAnonymously",task.getException());
+                            Toast.makeText(ImageLoadActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void controlAuth() {
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Log.e(TAG,"onAuthStateChanged: signed_in: "+user.getUid());
+                }else{
+                    Log.e(TAG,"onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        signIn();
+
+//        mAuth.signInAnonymously()
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        Log.e(TAG,"signInAnonymously:onComplete: "+ task.isSuccessful());
+//                        if(!task.isSuccessful()){
+//                            Log.e(TAG,"signInAnonymously",task.getException());
+//                            Toast.makeText(ImageLoadActivity.this, "Authentication failed.",
+//                                    Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
+
+
+
+
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        if(mAuthListener != null)
+            mAuth.removeAuthStateListener(mAuthListener);
     }
 
 }
